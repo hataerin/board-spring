b14c8c21425879b313f2f98d1c6ec943214a67b6
hataerin <goil1113@likelion.org>
2023-07-05 14:04:02 +0900

    user webflux적용중

--- src/main/java/board/demo/repository/UserRepository.java
+++ src/main/java/board/demo/repository/UserRepository.java
@@ -7,17 +7,4 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    // 회원 목록 조회
-
-    // 회원 상세 조회
-//    @Query(value = "select * from user where user_id = :id")
-//    public User findByUserId(@Param("id")Integer id);
-
-    // 회원 등록
-//    @Query(value = UserSQL.INSERT_USER)
-//    public User saveUser();
-
-    // 회원 수정
-
-    // 회원 삭제
 }
---
+++ src/main/java/board/demo/router/UserRouter.java
@@ -0,0 +1,32 @@
+package board.demo.router;
+
+import board.demo.handler.UserHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class UserRouter {
+
+    private final UserHandler userHandler;
+
+    public UserRouter(UserHandler userHandler) {
+        this.userHandler = userHandler;
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> userRoutes() {
+        return RouterFunctions.nest(RequestPredicates.path("/users"), // Base route path
+                RouterFunctions.nest(RequestPredicates.accept(MediaType.APPLICATION_JSON), // Basic Content-Type
+                        RouterFunctions.route(RequestPredicates.GET("/{userId}"), this.userHandler::getByUserId)
+                                .andRoute(RequestPredicates.PUT("/{userId}"), this.userHandler::updateUser)
+                                .andRoute(RequestPredicates.DELETE("/{userId}"), this.userHandler::deleteUser)
+                                .andRoute(RequestPredicates.method(HttpMethod.GET), this.userHandler::getAllUser)
+                                .andRoute(RequestPredicates.method(HttpMethod.POST), this.userHandler::saveUser)));
+    }
+}
