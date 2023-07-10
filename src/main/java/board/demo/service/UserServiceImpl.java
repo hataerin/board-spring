2d3c401fd4623563d88495cbabce4107b2fa939d
hataerin <goil1113@likelion.org>
2023-07-10 14:13:33 +0900

    cachePut 작업중

--- src/main/java/board/demo/service/UserServiceImpl.java
+++ src/main/java/board/demo/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +32,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Cacheable("user") // 캐시에 처음 데이터 넣으면 DB변경되어도 저장된 캐시만 가져옴
+    @Cacheable(value = "user", key = "'all'") // 캐시에 처음 데이터 넣으면 DB변경되어도 저장된 캐시만 가져옴
     @Transactional
     public List<UserAddressDto> findAllDetail() {
         log.warn("캐시적용: 전체조회");
@@ -48,14 +49,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    @CacheEvict(value = "user", allEntries = true) //메소드에서 어떤 파라미터를 받더라도 해당 캐시 데이터 모두 삭제
+    @CacheEvict(value = "user", key = "'all'")
     public void createUser(UserDto userDto) {
         userMapper.createUser(userDto);
     }
 
     @Override
-//    @CachePut(value = "user", key = "#id")
-    @CacheEvict(value = "user", allEntries = true)
+    @CachePut(value = "user", key = "#id")
+    @CacheEvict(value = "user", key = "'all'")
     @Transactional
     public void updateUser(Integer id,UserDto userDto) {
         userMapper.updateUser(id, userDto);
@@ -63,7 +64,10 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    @CacheEvict(value = "user", allEntries = true)
+    @Caching(evict = {
+            @CacheEvict(value = "user", key = "'all'"),
+            @CacheEvict(value = "user", key = "#id")
+    })
     @Transactional
     public void deleteUser(Integer id) {
         userMapper.deleteUser(id);
