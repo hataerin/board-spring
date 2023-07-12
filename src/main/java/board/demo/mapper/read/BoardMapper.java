08bc2d22b252edf2ca324aa4dfa0882688e73f23
hataerin <goil1113@likelion.org>
2023-07-12 10:22:35 +0900

    매퍼 수정중

---
+++ src/main/java/board/demo/mapper/read/BoardMapper.java
@@ -0,0 +1,29 @@
+package board.demo.mapper.read;
+
+import board.demo.model.Board;
+import board.demo.model.BoardDetailDto;
+import board.demo.model.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+
+    // 게시판 목록 조회
+    List<Board> findAll();
+
+    // 게시판 상세 조회
+    Board findById(Integer id);
+    BoardDetailDto findDetailById(Integer id);
+
+    // 게시판 등록
+    void createBoard(@Param("board") BoardDto board);
+
+    // 게시판 수정
+    void updateBoard(@Param("id") Integer id, @Param("board") BoardDto board);
+
+    // 게시판 삭제
+    void deleteBoard(@Param("id") Integer id);
+}
