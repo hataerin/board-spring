1e3b87f7c19b95081f227ac60f1e58a93b8c66cd
hataerin <goil1113@likelion.org>
2023-07-12 16:04:17 +0900

    redis적용중

--- src/main/java/board/demo/DemoApplication.java
+++ src/main/java/board/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package board.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableCaching
 public class DemoApplication {
 
     public static void main(String[] args) {
